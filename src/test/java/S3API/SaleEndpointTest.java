317e761b33c0a33e9f6e555678448c58b1e0b085
Bampie <baptist.o@outlook.com>
2022-10-24 16:25:45 +0200

    first setup for tests

---
+++ src/test/java/S3API/SaleEndpointTest.java
@@ -0,0 +1,15 @@
+package S3API;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+public class SaleEndpointTest {
+
+    @Test
+    public void TestGetAllSalesByProfileId() {
+        given()
+                .when().get("/api/sale/profileId/2d1693be-d360-4c6d-8ca7-1e898882fe44")
+                .then()
+                .statusCode(200);
+    }
+}
