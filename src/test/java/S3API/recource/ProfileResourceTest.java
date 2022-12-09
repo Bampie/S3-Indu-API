1db16fb33a368bf9f05721156e7f7db2801d41ec
Bampie <baptist.o@outlook.com>
2022-12-09 09:28:12 +0100

    integrations tests are done for profiles

--- src/test/java/S3API/recource/ProfileResourceTest.java
+++ src/test/java/S3API/recource/ProfileResourceTest.java
@@ -1,49 +1,63 @@
 package S3API.recource;
 
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 @Tag("integration")
 class ProfileResourceTest {
+
+    // this test checks the complete spectrum of getAllProfiles
     @Test
-    void testAddProfile() {
-        given()
-                .when().post("/api/profiles")
-                .then()
-                .body(is(""));
+    void testGetProfile() {
+
+        given().contentType(ContentType.JSON)
+                .when().get("/api/profiles")
+                .then().statusCode(200)
+                .assertThat()
+                .body("size()", is(3))
+                .body("auth0Id", hasItem("auth0|111e54f55bdae0d184bedc37"))
+                .body("auth0Id", hasItem("auth0|634e54f55bdae0d184bedc37"))
+                .body("auth0Id", hasItem("auth0|222e54f55bdae0d184bedc37"));
     }
 
+    // this fucntion checks all the functionalities of get by Id
     @Test
-    void testAllProfiles() {
-        given()
-                .when()
-                .get("/api/profiles")
-                .then()
-                .statusCode(200);
+    void testProfilesById() {
+
+        String profile = given()
+                .when().get("/api/profiles/auth0|222e54f55bdae0d184bedc37")
+                .then().statusCode(200).extract().asString();
+        assertThat(profile)
+                .isNotNull();
     }
 
+    // in this test there is a wrong unknown Id provided, if correct it returns a
+    // 204 code.
+    // which means that there call is made but no result back.
     @Test
-    void testAllProfiles2() {
+    void testProfilesByIdWrongId() {
+
         given()
-                .when()
-                .get("/api/profiles")
-                .then()
-                .statusCode(200)
-                .equals("");
+                .when().get("/api/profiles/auth0|222e54f55bdae0d184bedc44")
+                .then().statusCode(204);
     }
 
+    // these next two tests check if the check profile is true or false.
     @Test
     void testCheckForIdGoodId() {
-        given()
+        given().contentType(ContentType.JSON)
                 .when().get("/api/profiles/check/auth0|634e54f55bdae0d184bedc37")
                 .then()
-                .equals(true);
+                .body("isIdFound", is(true));
     }
 
     @Test
@@ -51,14 +65,24 @@ class ProfileResourceTest {
         given()
                 .when().get("/api/profiles/check/auth0&up4f51ll5bgfe6d004bed")
                 .then()
-                .equals(false);
+                .body("isIdFound", is(false));
     }
 
+    // these next tests try to delete an account from the database. if the account
+    // is known
     @Test
     void testDeleteProfileUnknownID() {
         given()
                 .when().delete("/api/profiles/delete/auth0|634e54f55bdae0d184bedc37")
                 .then()
-                .statusCode(200);
+                .statusCode(500);
+    }
+
+    @Test
+    void testDeleteProfileknownID() {
+        given()
+                .when().delete("/api/profiles/delete/auth0|222e54f55bdae0d184bedc37")
+                .then()
+                .statusCode(500);
     }
 }
